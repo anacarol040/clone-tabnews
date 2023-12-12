85858bde2a04bf11a262c67072a86228f9324b85
Ana de Oliveira <anacarol040@gmail.com>
2023-12-12 14:57:10 -0700

    fix format

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Praticando do zero
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-function Home () {
-    return <h1>Vai para a academia hoje? Se vc não for, só vc não vai</h1>
+function Home() {
+  return <h1>Vai para a academia hoje? Se vc não for, só vc não vai</h1>;
 }
-
 export default Home;
